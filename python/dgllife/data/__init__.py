411277757b51b8787e232debab4e35035494d17c
Jinyu Hou <jinyu.hou@mail.utoronto.ca>
2020-11-14 16:53:35 +0800

    disabled all import in dgllife/data/__init__.py

--- python/dgllife/data/__init__.py
+++ python/dgllife/data/__init__.py
@@ -4,22 +4,22 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # from .alchemy import *
-from .astrazeneca_chembl_solubility import *
-from .csv_dataset import *
-from .smiles_inference import *
-from .pdbbind import *
-from .pubchem_aromaticity import *
-from .tox21 import *
-from .esol import *
-from .freesolv import *
-from .lipophilicity import *
-from .pcba import *
-from .muv import *
-from .hiv import *
-from .bace import *
-from .bbbp import *
-from .toxcast import *
-from .sider import *
-from .clintox import *
-from .uspto import *
-from .jtvae import *
+# from .astrazeneca_chembl_solubility import *
+# from .csv_dataset import *
+# from .smiles_inference import *
+# from .pdbbind import *
+# from .pubchem_aromaticity import *
+# from .tox21 import *
+# from .esol import *
+# from .freesolv import *
+# from .lipophilicity import *
+# from .pcba import *
+# from .muv import *
+# from .hiv import *
+# from .bace import *
+# from .bbbp import *
+# from .toxcast import *
+# from .sider import *
+# from .clintox import *
+# from .uspto import *
+# from .jtvae import *
