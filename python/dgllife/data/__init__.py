e6d15c8d42a82004d9dda5a3bc315a489c455110
Jinyu Hou <jinyu.hou@mail.utoronto.ca>
2020-11-14 16:58:51 +0800

    recovered all imports indgllife/data/__init__.py; disabled from . import data, utils in dgllife/I__init__.py

--- python/dgllife/data/__init__.py
+++ python/dgllife/data/__init__.py
@@ -3,23 +3,23 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-# from .alchemy import *
-# from .astrazeneca_chembl_solubility import *
-# from .csv_dataset import *
-# from .smiles_inference import *
-# from .pdbbind import *
-# from .pubchem_aromaticity import *
-# from .tox21 import *
-# from .esol import *
-# from .freesolv import *
-# from .lipophilicity import *
-# from .pcba import *
-# from .muv import *
-# from .hiv import *
-# from .bace import *
-# from .bbbp import *
-# from .toxcast import *
-# from .sider import *
-# from .clintox import *
-# from .uspto import *
-# from .jtvae import *
+from .alchemy import *
+from .astrazeneca_chembl_solubility import *
+from .csv_dataset import *
+from .smiles_inference import *
+from .pdbbind import *
+from .pubchem_aromaticity import *
+from .tox21 import *
+from .esol import *
+from .freesolv import *
+from .lipophilicity import *
+from .pcba import *
+from .muv import *
+from .hiv import *
+from .bace import *
+from .bbbp import *
+from .toxcast import *
+from .sider import *
+from .clintox import *
+from .uspto import *
+from .jtvae import *
