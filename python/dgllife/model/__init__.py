1e4587ebf935ac69e1ea938ac300beccf6664370
Jinyu Hou <jinyu.hou@mail.utoronto.ca>
2021-02-19 20:01:37 +0800

    disabled model/init import

--- python/dgllife/model/__init__.py
+++ python/dgllife/model/__init__.py
@@ -3,7 +3,7 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-from .gnn import *
-from .readout import *
+# from .gnn import *
+# from .readout import *
 from .model_zoo import *
-from .pretrain import *
+# from .pretrain import *
