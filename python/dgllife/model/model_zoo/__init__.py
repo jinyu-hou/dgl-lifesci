f33b2fd94d64a35fe0656c52242a5c396a785e50
Jinyu Hou <jinyu.hou@mail.utoronto.ca>
2021-02-19 20:02:52 +0800

    disabled model/model_zoo/init import

--- python/dgllife/model/model_zoo/__init__.py
+++ python/dgllife/model/model_zoo/__init__.py
@@ -6,19 +6,19 @@
 # Collection of model architectures
 
 # Molecular property prediction
-from .attentivefp_predictor import *
-from .gat_predictor import *
-from .gcn_predictor import *
-from .gin_predictor import *
-from .mgcn_predictor import *
-from .mlp_predictor import *
-from .mpnn_predictor import *
-from .schnet_predictor import *
-from .weave_predictor import *
-from .gnn_ogb_predictor import *
+# from .attentivefp_predictor import *
+# from .gat_predictor import *
+# from .gcn_predictor import *
+# from .gin_predictor import *
+# from .mgcn_predictor import *
+# from .mlp_predictor import *
+# from .mpnn_predictor import *
+# from .schnet_predictor import *
+# from .weave_predictor import *
+# from .gnn_ogb_predictor import *
 
 from .dgmg import *
-from .jtnn import *
+# from .jtnn import *
 # # Generative models
 # try:
 #     # DGMG requires RDKit support
